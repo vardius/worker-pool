bb5173060233ec90ba0c6d0c919b3b4abc81ed0f
Rafał Lorenz <vardius@gmail.com>
2017-10-15 17:03:12 +1100

    Add bench result to redme

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,26 +1,16 @@
 package workerpool
 
-import (
-	"bytes"
-	"testing"
-)
+import "testing"
 
 func BenchmarkWorker(b *testing.B) {
 	pool := New(2)
 	defer pool.Stop()
 
-	out := make(chan int, 3)
-	defer close(out)
-
-	pool.Start(2, func(i int) {
-		out <- i
-	})
+	pool.Start(2, func(i int) {})
 
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
-		var buf bytes.Buffer
 		for pb.Next() {
-			buf.Reset()
 			pool.Delegate(1)
 		}
 	})
